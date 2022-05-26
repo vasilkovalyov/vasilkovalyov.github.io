f953ef73b96c7e65477883e68ef5a9947b2b1f72
Vasiliy Kovalyov <vasiliy.kovalyov@ukad-group.com>
2022-05-26 22:21:41 +0300

    Temp commit

--- components/theme/plain/Grid/Row.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { IRow } from '../../../../interfaces/common'
-
-export default function Row({ className, children, alignItems, justify }: IRow) {
-  const mediaClasses = (justify ? `${justify}` : '') + (alignItems ? ` ${['align-' + alignItems]}` : '')
-
-  return <div className={`row ${mediaClasses} ${className ? className : ''}`}>{children}</div>
-}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    const defaultLocale = `/${process.env.NEXT_PUBLIC_DEFAULT_LOCALE || 'en'}`
+    return [
+      {
+        source: '/',
+        destination: defaultLocale,
+        permanent: true,
+      },
+    ]
+  },
 }
